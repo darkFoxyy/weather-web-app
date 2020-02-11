157ab4e2560b869e5168d724f951813c6de1b845
darkFoxyy <dragoha2000@gmail.com>
2020-02-11 19:11:08 +0300

    Fixed errors

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,9 @@
 document.addEventListener('submit', (event) => {
     event.preventDefault()
     const data = document.querySelector('form input').value
-    
+
     fetch(`http://localhost:3000/data?location=${data}`, { method: 'GET' }).then(async (res) => {
-        const data = await res.json()      
-        document.getElementById('1').innerHTML = data.data
+        const data = await res.json()    
+        document.getElementById('1').innerHTML = data.weatherData
     })
 })
